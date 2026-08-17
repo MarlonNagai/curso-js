6e949cd75bb55b7a879dc123e59873e94fcb809d
Marlon  Nagai <nagaimarlon@gmail.com>
2026-08-17 20:39:11 -0300

    terminando o exercicio

--- cursojs/aula12ex/ex014/script.js
+++ cursojs/aula12ex/ex014/script.js
@@ -1,1 +1,17 @@
-window.alert('olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora =data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora < 18) {
+        img.src = 'fototarde.png'
+        document.body.style.background = '#b54f27'
+    } else {
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+}
