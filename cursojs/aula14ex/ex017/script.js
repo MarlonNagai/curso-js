384e312fc0ebca12041587ee7059d37d32df9d0d
Marlon  Nagai <nagaimarlon@gmail.com>
2026-08-23 17:05:45 -0300

    exercicio 6

--- cursojs/aula14ex/ex017/script.js
+++ cursojs/aula14ex/ex017/script.js
@@ -1,1 +1,10 @@
-window.alert('olá')
+function contar() {
+    var n = window.document.getElementById('tabuada')
+    var res = window.document.getElementById('seltab')
+    num = Number(n.value)
+
+    for (var c = 1; c <= 10; c++ ){
+        res.innerHTML += ` ${num} x ${c} = ${num * c} </br>` 
+    }
+    res.innerHTML += `FIM!`
+}
