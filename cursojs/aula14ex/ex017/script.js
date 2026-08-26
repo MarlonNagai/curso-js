32580d101731cf6d6180f18670cca0305563c9ea
Marlon  Nagai <nagaimarlon@gmail.com>
2026-08-25 21:11:08 -0300

    corrigindo o exercicio

--- cursojs/aula14ex/ex017/script.js
+++ cursojs/aula14ex/ex017/script.js
@@ -1,10 +1,15 @@
 function contar() {
     var n = window.document.getElementById('tabuada')
     var res = window.document.getElementById('seltab')
-    num = Number(n.value)
-
+    if (n.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else
+        num = Number(n.value)
+    res.innerHTML = ''
     for (var c = 1; c <= 10; c++ ){
-        res.innerHTML += ` ${num} x ${c} = ${num * c} </br>` 
+        var item = document.createElement('option')
+        item.text = ` ${num} x ${c} = ${num * c}`
+        item.value = `tab${c}`
+        res.appendChild(item)
     }
-    res.innerHTML += `FIM!`
 }
