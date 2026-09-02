7f916cd12069f6652503a814acf2fecb94aa1718
Marlon  Nagai <nagaimarlon@gmail.com>
2026-09-02 20:09:43 -0300

    exercicio 7

---
+++ cursojs/aula16ex/modelo/script.js
@@ -0,0 +1,27 @@
+var num = document.querySelector('input#fnum')
+var lista = document.querySelector('select#flista')
+var res = document.querySelector('div#res')
+var valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+    } else {
+        window.alert('valor Invalido ou ja encontrado na lista.')
+    }
+}
